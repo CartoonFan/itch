c7587aa37c69be35eb150d5c522a8ee932ec90f3
leaf corcoran <leafot@gmail.com>
2026-05-05 16:01:08 -0700

    bump electron & co.

--- integration-tests/versions.go
+++ integration-tests/versions.go
@@ -1,4 +1,4 @@
 package main
 
-const electronVersion = "41.3.0"
-const chromeDriverVersionString = "ChromeDriver 146.0.7680.188"
+const electronVersion = "41.5.0"
+const chromeDriverVersionString = "ChromeDriver 146.0.7680.216"
